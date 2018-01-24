d483f919fa959b7288a189f1176347307715cdff
Rubiooo <maplerubiooo>
2018-01-24 14:49:35 -0500

    origin - start the project - MyDribbbooo

---
+++ app/src/main/java/com/example/rubioo/mydribbbooo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rubioo.mydribbbooo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
