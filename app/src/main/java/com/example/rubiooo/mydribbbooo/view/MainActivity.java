0eb80a6ca96cc74c43287aa855aded25e9c18ca2
Rubiooo <maplerubiooo>
2018-01-26 15:00:53 -0500

    MainActivity

---
+++ app/src/main/java/com/example/rubiooo/mydribbbooo/view/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.rubiooo.mydribbbooo.view;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.rubioo.mydribbbooo.R;
+import com.example.rubiooo.mydribbbooo.view.shot_list.ShotListAdapter;
+import com.example.rubiooo.mydribbbooo.view.shot_list.ShotListFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Jimmy on 2018/1/25.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.drawer) NavigationView navigationView;
+
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        setupDrawer();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, ShotListFragment.newInstance())
+                    .commit();
+        }
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig ) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(
+                this,          /*host activity*/
+                drawerLayout,          /* drawer layout object*/
+                R.string.open_drawer,  /* 'open drawer' description*/
+                R.string.close_drawer  /* "close drawer' description*/
+        );
+
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.isChecked()) {
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+
+                Fragment fragment = null;
+                switch(item.getItemId()) {
+                    case R.id.drawer_item_home:
+                        fragment = ShotListFragment.newInstance();
+                        setTitle(R.string.title_home);
+                        break;
+                    case R.id.drawer_item_likes:
+                        fragment = ShotListFragment.newInstance();
+                        setTitle(R.string.title_like);
+                        break;
+                    case R.id.drawer_item_buckets:
+                        fragment = ShotListFragment.newInstance();
+                        setTitle(R.string.title_bucket);
+                        break;
+                }
+
+                drawerLayout.closeDrawers();
+
+                if (fragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+}
+
